3a69897853771002e6e20216d2ea34eaf13350d0
Eugen Mayer <Dyubomayer@gmail.com>
2021-03-06 21:32:57 +0500

    add test

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -27,6 +27,6 @@ class MobileBankApiTestV3 {
           .body("", hasSize(3))
           .body("[0].currency", equalTo("RUB"))
           .body("[0].balance", greaterThanOrEqualTo(0))
-      ;
+              .body("[2].name",equalTo("Текущий зарплатный счёт"));
     }
 }
